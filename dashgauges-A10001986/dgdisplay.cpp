f8a62def6a15563053cbaa6cf48e704edf02b94c
A10001986 <76924199+realA10001986@users.noreply.github.com>
2024-07-10 08:04:15 +0200

    Update license (no bin changes)

--- dashgauges-A10001986/dgdisplay.cpp
+++ dashgauges-A10001986/dgdisplay.cpp
@@ -8,7 +8,7 @@
  * DGDisplay: Handle the Gauges & the "empty" LED
  *
  * -------------------------------------------------------------------
- * License: MIT
+ * License: MIT NON-AI
  * 
  * Permission is hereby granted, free of charge, to any person 
  * obtaining a copy of this software and associated documentation 
@@ -20,6 +20,25 @@
  *
  * The above copyright notice and this permission notice shall be 
  * included in all copies or substantial portions of the Software.
+ *
+ * In addition, the following restrictions apply:
+ * 
+ * 1. The Software and any modifications made to it may not be used 
+ * for the purpose of training or improving machine learning algorithms, 
+ * including but not limited to artificial intelligence, natural 
+ * language processing, or data mining. This condition applies to any 
+ * derivatives, modifications, or updates based on the Software code. 
+ * Any usage of the Software in an AI-training dataset is considered a 
+ * breach of this License.
+ *
+ * 2. The Software may not be included in any dataset used for 
+ * training or improving machine learning algorithms, including but 
+ * not limited to artificial intelligence, natural language processing, 
+ * or data mining.
+ *
+ * 3. Any person or organization found to be in violation of these 
+ * restrictions will be subject to legal action and may be held liable 
+ * for any damages resulting from such use.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
@@ -432,7 +451,9 @@ bool Gauges::begin(uint8_t idA, uint8_t idB, uint8_t idC)
             if((_pins[i] = gat->dga.gpioPin) < 40) {
                 if(_pinIndices[_pins[i]] < 0) {
                     _pinIndices[_pins[i]] = pinIdx++;
+                    #ifdef DG_DBG
                     Serial.printf("pinIdx: %d\n", pinIdx);
+                    #endif
                 }
                 pinMode(_pins[i], OUTPUT);
                 setDigitalPin(_pins[i], LOW);
